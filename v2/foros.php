2b9e2c9abd386583446d390caae6240331c258d6
Juan Carrión <6473248+juancrrn@users.noreply.github.com>
2020-04-02 01:25:13 +0200

    Update foros.php

--- v2/foros.php
+++ v2/foros.php
@@ -20,3 +20,25 @@
  */
 
 require_once __DIR__ . "/sistema/configuracion.php";
+
+use \Awsw\Gesi\Vistas\Vista;
+
+Vista::setPaginaActual("Foros", "foros");
+
+Vista::incluirCabecera();
+
+?>
+<div class="wrapper">
+	<div class="container">
+		<header class="page-header">
+			<h1>Foros</h1>
+		</header>
+
+		<section class="page-content">
+			...
+		</section>
+	</div>
+</div>
+<?php
+
+Vista::incluirPie();
