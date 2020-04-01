048a89cee84981a60bdc3ed2f8fd9752095b94e1
Juan Carrión <6473248+juancrrn@users.noreply.github.com>
2020-04-02 01:24:32 +0200

    Update calendario.php

--- v2/calendario.php
+++ v2/calendario.php
@@ -22,3 +22,25 @@
  */
 
 require_once __DIR__ . "/sistema/configuracion.php";
+
+use \Awsw\Gesi\Vistas\Vista;
+
+Vista::setPaginaActual("Calendario", "calendario");
+
+Vista::incluirCabecera();
+
+?>
+<div class="wrapper">
+	<div class="container">
+		<header class="page-header">
+			<h1>Calendario</h1>
+		</header>
+
+		<section class="page-content">
+			...
+		</section>
+	</div>
+</div>
+<?php
+
+Vista::incluirPie();
