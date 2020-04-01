79a5ca35088561ac0c48f23e531a7d1ae3314d82
Juan Carrión <6473248+juancrrn@users.noreply.github.com>
2020-04-02 01:26:17 +0200

    Update mensajes.php

--- v2/mensajes.php
+++ v2/mensajes.php
@@ -21,3 +21,25 @@
  */
 
 require_once __DIR__ . "/sistema/configuracion.php";
+
+use \Awsw\Gesi\Vistas\Vista;
+
+Vista::setPaginaActual("Mensajes de secretaría", "mensajes");
+
+Vista::incluirCabecera();
+
+?>
+<div class="wrapper">
+	<div class="container">
+		<header class="page-header">
+			<h1>Mensajes de secretaría</h1>
+		</header>
+
+		<section class="page-content">
+			...
+		</section>
+	</div>
+</div>
+<?php
+
+Vista::incluirPie();
